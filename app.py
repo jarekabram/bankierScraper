b622acce471c9b3586ca58a035e2b26b709edeba
Jarek Abram <jarek.abram1@gmail.com>
2019-11-28 10:50:33 +0100

    added creation of subprocess in flask to run spider using scrapy

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, escape, request
 import sys
+import subprocess
 sys.path.append(".")
 import scraper
 
@@ -8,6 +9,6 @@ app = Flask(__name__)
 @app.route('/')
 def welcome():
     name = request.args.get("name", "Welcome to bankier scraping website")
-    scraper_obj = Scraper()
-    scraper_obj.parse()
+    spider_name = "scraper\\scraper.py"
+    subprocess.check_output(['scrapy', 'runspider', spider_name])
     return f'{escape(name)}!'
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -2,8 +2,18 @@ import scrapy
 
 class Scraper(scrapy.Spider):
     name = 'bankier'
-    pages = ['rynki , gielda , gospodarka , surowce  , fundusze , narzedzia , wiadomosci']
-    start_urls = ['https://www.bankier.pl/']
+    pages = ['rynki' , 'gielda' , 'gospodarka' , 'surowce' , 'fundusze' , 'narzedzia' , 'wiadomosci']
+    subpage = 'wiadomosci'
+    start_url = 'https://www.bankier.pl/'
+
+    def start_requests(self):
+        urls = []
+        for page in self.pages:
+            full_website = self.start_url + page
+            urls.append(full_website)
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
 
     def parse(self, response):
         for title in response.css('.post-header>h2'):
